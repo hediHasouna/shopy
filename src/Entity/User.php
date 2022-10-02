3b17b7498421f8910e5e9db339bd83f96121323d
Hedi <business2060@gmail.com>
2022-10-02 19:08:45 +0200

    fixing databse issue 2

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -9,7 +9,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
- * @ORM\Table(name="utilisateur")
+ * @ORM\Table(name="utilisateurs")
  */
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
